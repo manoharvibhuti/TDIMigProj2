e086d7dfecd5c8013229a273e7f6d3167052d172
tdiadmin <tdiadmin@tdimbvdev>
2018-02-27 11:35:21 +0000

    Add project files.

---
+++ TDIMigProj2/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TDIMigProj2.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TDIMigProj2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDIMigProj2.Startup))]
+namespace TDIMigProj2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
